b77c36b5cee3975564787c244a69d38f3ab9c2c7
Eunbi Go <eunbigo91@gmail.com>
2016-12-22 17:52:59 -0800

    Add: NYT article result when a user searches specific city

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -11,15 +11,28 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    // load streetview
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-    // load streetview
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // NYTimes AJAX request
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&sort=newest&api-key=7fd98707f4f54137bbd7a08a4ffe6e57';
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        articles = data.response.docs;
+        for(var i=0; i < articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>'+article.snippet+'</p>'+
+                '<li>');
+        };
+    });
 
     return false;
 };
