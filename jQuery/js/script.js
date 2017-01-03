d339e6a4bebf2bbf5fd4da93f4b54ea6ea9f260d
Eunbi Go <eunbigo91@gmail.com>
2017-01-03 11:30:29 -0800

    Add: cats object to display two cats and increase the num of clicks

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,10 +1,31 @@
 
-$(document).ready(function() {
+var cats =[
+    {
+        "name" : "cat1",
+        "pic" : "img/cat1.jpg",
+        "clicks" : 0
+    },
+    {
+        "name" : "cat2",
+        "pic" : "img/cat2.jpg",
+        "clicks" : 0
+    }
+];
 
-    var counter = 0;
-    $(".count").append("<p id='clicks'>" + counter.toString() + "</p>");
-    $('#cat_image').click(function(){
-        counter += 1;
-        $('#clicks').text(counter.toString());
+function makeCatRows() {
+    var toAdd = "";
+    $.each(cats, function(catIndex, cat) {
+        toAdd += "<div class='cat'><div class='name'>" + cat.name + "</div><img src='" + cat.pic + "' class='cat_img'/><div id='" + catIndex.toString() + "' class='count'>" + cat.clicks.toString() + "</div></div>";
+    });
+    $("#main").append("<div class='row'>" + toAdd + "</div>");
+}
+
+$(document).ready(function() {
+    makeCatRows();
+    $(".cat_img").click(function(obj) {
+        var elem = obj.target.parentElement.childNodes[2];
+        console.log(elem);
+        cats[elem.id].clicks += 1;
+        $("#" + elem.id).text(cats[elem.id].clicks);
     });
 });
