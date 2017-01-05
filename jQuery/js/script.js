e80db1645e8428b3a84ee1e385995c398e6a5ae7
Eunbi Go <eunbigo91@gmail.com>
2017-01-04 18:09:15 -0800

    Add: 3 cats in object cats (total 5 cats)

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -9,6 +9,21 @@ var cats =[
         "name" : "cat2",
         "pic" : "img/cat2.jpg",
         "clicks" : 0
+    },
+    {
+        "name" : "cat3",
+        "pic" : "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg",
+        "clicks" : 0
+    },
+    {
+        "name" : "cat4",
+        "pic" : "https://s-media-cache-ak0.pinimg.com/564x/b4/c7/54/b4c754c9b93669f25e922a06644bd88e.jpg",
+        "clicks" : 0
+    },
+    {
+        "name" : "cat5",
+        "pic" : "https://s-media-cache-ak0.pinimg.com/236x/ec/a3/2e/eca32ee5c3c2c768704a7198d60b867d.jpg",
+        "clicks" : 0
     }
 ];
 
