aa4503605fa51c14a8fa988698d2beeb4c960865
Eunbi Go <eunbigo91@gmail.com>
2017-01-09 13:09:51 -0800

    Change: cat clicker codes using the MVC design pattern

--- jQuery/js/script.js
+++ jQuery/js/script.js
@@ -1,56 +1,99 @@
 
-var cats =[
-    {
-        "name" : "cat1",
-        "pic" : "img/cat1.jpg",
-        "clicks" : 0
+/* ======= Model ======= */
+
+var model = {
+    currentCat: null,
+    cats: [
+        {
+            "name" : "cat1",
+            "pic" : "img/cat1.jpg",
+            "clicks" : 0
+        },
+        {
+            "name" : "cat2",
+            "pic" : "img/cat2.jpg",
+            "clicks" : 0
+        },
+        {
+            "name" : "cat3",
+            "pic" : "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg",
+            "clicks" : 0
+        },
+        {
+            "name" : "cat4",
+            "pic" : "https://s-media-cache-ak0.pinimg.com/564x/b4/c7/54/b4c754c9b93669f25e922a06644bd88e.jpg",
+            "clicks" : 0
+        },
+        {
+            "name" : "cat5",
+            "pic" : "https://s-media-cache-ak0.pinimg.com/236x/ec/a3/2e/eca32ee5c3c2c768704a7198d60b867d.jpg",
+            "clicks" : 0
+        }
+    ],
+};
+
+/* ======= Octopus ======= */
+
+var octopus = {
+    init: function() {
+        model.currentCat = model.cats[0];
+
+        catListView.init();
+        catView.init();
     },
-    {
-        "name" : "cat2",
-        "pic" : "img/cat2.jpg",
-        "clicks" : 0
+
+    getCurrentCat: function() {
+        return model.currentCat;
     },
-    {
-        "name" : "cat3",
-        "pic" : "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg",
-        "clicks" : 0
+
+    getCats: function() {
+        return model.cats;
     },
-    {
-        "name" : "cat4",
-        "pic" : "https://s-media-cache-ak0.pinimg.com/564x/b4/c7/54/b4c754c9b93669f25e922a06644bd88e.jpg",
-        "clicks" : 0
+
+    setCurrentCat: function(index) {
+        model.currentCat = model.cats[index];
     },
-    {
-        "name" : "cat5",
-        "pic" : "https://s-media-cache-ak0.pinimg.com/236x/ec/a3/2e/eca32ee5c3c2c768704a7198d60b867d.jpg",
-        "clicks" : 0
+
+    incrementCounter: function() {
+        model.currentCat.clicks++;
+        catView.render();
     }
-];
+};
 
-function makeACatList(){
-    var toAdd = "";
-    $.each(cats, function(catIndex, cat) {
-        toAdd += "<li class='cat list'>"+cat.name+"</li>";
-    });
-    $("#list").append("<ul class='cat_list'>"+toAdd+"</ul>");
-}
 
-function displaySelectedCat(id){
-    $("#display").empty();
-    cat = cats[id];
-    var toDisplay = "<div class='name'>" + cat.name + "</div><img src='" + cat.pic + "' class='cat_img'/><div id='" + id.toString() + "' class='count'>" + cat.clicks.toString() + "</div>";
-    $("#display").append(toDisplay);
-    $(".cat_img").click(function(object) {
-        var elem = object.target.parentElement.childNodes[2];
-        cats[elem.id].clicks += 1;
-        $("#" + elem.id).text(cats[elem.id].clicks);
-    });
-}
+/* ======= View ======= */
 
-$(document).ready(function(){
-    makeACatList();
-    $(".cat").click(function(obj) {
-        id = cats.indexOf(cats.filter(function(a){ return a.name == obj.target.innerText; })[0]);
-        displaySelectedCat(id);
-    });
-});
+var catView = {
+    init: function() {
+        $('#cat-img').click(function() {
+            octopus.incrementCounter();
+        });
+        this.render();
+    },
+
+    render: function() {
+        var currentCat = octopus.getCurrentCat();
+        $('#cat-count').text(currentCat.clicks);
+        $('#cat-name').text(currentCat.name);
+        $('#cat-img').attr("src", currentCat.pic);
+    }
+};
+
+var catListView = {
+    init: function() {
+        var cats = octopus.getCats();
+        var toAdd = "";
+        $.each(cats, function(catIndex, cat) {
+            toAdd += "<li class='cat-index list'>"+cat.name+"</li>";
+        });
+        $("#cat-list").append(toAdd);
+
+        $('.cat-index').click(function(obj) {
+            index = cats.indexOf(cats.filter(function(a){ return a.name == obj.target.innerText; })[0]);
+            octopus.setCurrentCat(index);
+            catView.render();
+        });
+    }
+};
+
+octopus.init();
