2392223f37b658b5bb278d153b93d3ee2440d2d6
Eunbi Go <eunbigo91@gmail.com>
2017-01-16 16:08:33 -0800

    Add: title and nicknames using ko.computed and observableArray

--- knockout/js/app.js
+++ knockout/js/app.js
@@ -3,6 +3,20 @@ var ViewModel = function() {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttibution = ko.observable('https://www.flickr.com/photos/');
+    this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T']);
+    this.title = ko.computed(function(){
+        if (this.clickCount() < 10) {
+            return 'Newborn';
+        } else if (this.clickCount() < 20) {
+            return 'Infant';
+        } else if (this.clickCount() < 50) {
+            return 'Child';
+        } else if (this.clickCount() < 100) {
+            return 'Teen';
+        } else {
+            return 'Adult';
+        }
+    }, this);
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
