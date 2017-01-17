298f401cdd3334ebdc413a5ec2cc20a6b09921ec
Eunbi Go <eunbigo91@gmail.com>
2017-01-16 17:31:24 -0800

    Add: 5 cats and cat list
    A cat image and title, clickCount will change if user clicks cat's name

--- knockout/js/app.js
+++ knockout/js/app.js
@@ -1,9 +1,43 @@
-var Cat = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttibution = ko.observable('https://www.flickr.com/photos/');
-    this.nicknames = ko.observableArray(['Tabtab', 'T-Bone', 'Mr. T']);
+var initialCats = [
+    {
+        'name' : 'Tabby',
+        'imgSrc' : 'img/22252709_010df3379e_z.jpg',
+        'clickCount' : 0,
+        'imgAttribution' : 'https://www.flickr.com/photos/bigtallguy/434164568',
+        'nicknames' : ['Tabtab', 'T-Bone', 'Mr. T']
+    },
+    {
+        'name' : 'Tiger',
+        'imgSrc' : 'img/434164568_fea0ad4013_z.jpg',
+        'clickCount' : 0,
+        'nicknames' : ['Tigger']
+    },
+    {
+        'name' : 'Scaredy',
+        'imgSrc' : 'img/1413379559_412a540d29_z.jpg',
+        'clickCount' : 0,
+        'nicknames' : ['Casper']
+    },
+    {
+        'name' : 'Shadow',
+        'imgSrc' : 'img/4154543904_6e2428c421_z.jpg',
+        'clickCount' : 0,
+        'nicknames' : ['Shooby']
+    },
+    {
+        'name' : 'Sleepy',
+        'imgSrc' : 'img/9648464288_2516b35537_z.jpg',
+        'clickCount' : 0,
+        'nicknames' : ['Zzzzzz']
+    }
+]
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttibution);
+    this.nicknames = ko.observableArray(data.nicknames);
+
     this.title = ko.computed(function(){
         if (this.clickCount() < 10) {
             return 'Newborn';
@@ -21,7 +55,13 @@ var Cat = function() {
 
 var ViewModel = function() {
     var self = this;
-    this.currentCat = ko.observable(new Cat());
+
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem) );
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
